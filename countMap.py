514a83ed9bd906f1f961a5e0f2d87887eac14dfc
Thomas Lynch <tlynch3@disc01.crc.nd.edu>
2021-04-20 11:40:49 -0400

    Editing parser and mappers

--- countMap.py
+++ countMap.py
@@ -11,9 +11,5 @@ for count, line in enumerate(stream):
     if count == 0:
         continue
     row_entry = line.split(",")
-    if row_entry[0][0] == " ":
-        row_entry[0] = row_entry[0][1:]
-    if row_entry[0][-1] == " ":
-        row_entry[0] = row_entry[0][:-1]
     
     print(str(row_entry[d_col]) + "\0" + str(1))
--- parseMap.py
+++ parseMap.py
@@ -8,6 +8,10 @@ stream = io.TextIOWrapper(sys.stdin.buffer, encoding='iso-8859-1')
 
 for line in stream:
     row_entry = line.split(",")
+    if row_entry[0][0] == " ":
+        row_entry[0] = row_entry[0][1:]
+    if row_entry[0][-1] == " ":
+        row_entry[0] = row_entry[0][:-1]
     row_entry = row_entry[:-1]
     new_string = ""
     for count, term in enumerate(row_entry):
