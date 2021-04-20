40e8805c74746be5bb79809a66f142f98c4c4df6
Thomas Lynch <tlynch3@disc01.crc.nd.edu>
2021-04-19 21:36:47 -0400

    Adding minmax map and reduce

---
+++ minmaxMap.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import io
+
+
+stream = io.TextIOWrapper(sys.stdin.buffer, encoding='iso-8859-1')
+
+d_col = int(sys.argv[1])
+minmax_col = int(sys.argv[2])
+for count, line in enumerate(stream):
+    if count == 0:
+        continue
+    row_entry = line.split(",")
+    if row_entry[0][0] == " ":
+        row_entry[0] = row_entry[0][1:]
+    if row_entry[0][-1] == " ":
+        row_entry[0] = row_entry[0][:-1]
+    
+    print(str(row_entry[d_col]) + "\0" + str(row_entry[minmax_col]))
---
+++ minmaxReduce.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import sys
+from operator import itemgetter
+
+table = {}
+
+minmax = int(sys.argv[1])
+order = int(sys.argv[2])
+sort_type = int(sys.argv[3])
+limit = int(sys.argv[4])
+
+for line in sys.stdin:
+    d_col, minmax_col = line.split("\0")
+
+    if d_col in table:
+        try:
+            if minmax == 0:
+                if int(minmax_col) < table[d_col]:
+                    table[d_col] = int(minmax_col)
+            else:
+                if int(minmax_col) > table[d_col]:
+                    table[d_col] = int(minmax_col)
+        except:
+            pass
+    else:
+        try:
+            table[d_col] = int(minmax_col)
+        except:
+            pass
+
+if order == 0:
+    count = 0
+    if sort_type == 0:
+        for key, value in sorted(table.items(), key=itemgetter(0), reverse=False):
+            if limit < 0 or count < limit:
+                print(key, value)
+                count += 1
+    else:
+        for key, value in sorted(table.items(), key=itemgetter(0), reverse=True):
+            if limit < 0 or count < limit:
+                print(key, value)
+                count += 1
+else:
+    count = 0
+    if sort_type == 0:
+        for key, value in sorted(table.items(), key=itemgetter(1), reverse=False):
+            if limit < 0 or count < limit:
+                print(key, value)
+                count += 1
+    else:
+        for key, value in sorted(table.items(), key=itemgetter(1), reverse=True):
+            if limit < 0 or count < limit:
+                print(key, value)
+                count += 1
